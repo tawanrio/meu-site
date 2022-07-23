f25f1887d66564bd5f88e857394c89139391336b
tawanrio <stawanrio@gmail.com>
2022-07-22 21:07:40 -0300

    22/07

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,10 @@ const appView = new AppView()
 const appService = new AppService()
 const appController = new AppController(appView, appService)
 
+
+ 
+
+
 document.querySelector('form#whats').addEventListener('submit', function(e){
    e.preventDefault();
    const name = this.name.value
@@ -17,7 +21,7 @@ document.querySelectorAll('.btn-toggle').forEach(element => {
    element.addEventListener('click', function(){
       document.querySelector('.whats').classList.toggle('whats-toggle')
       if(!document.querySelector('.whats').classList.contains('whats-toggle')){
-         return document.querySelector('#whats-span').innerHTML = 'WhatsApp' 
+         return document.querySelector('#whats-span').innerHTML = 'WhatsApp <i class="fa-brands fa-whatsapp"></i>' 
       }
       return document.querySelector('#whats-span').innerHTML = 'Fale Comigo' 
    })
